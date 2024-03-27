7a884c66177eec3161d139e73a55ca14ebe0aca9
Daniela Baron <baron.daniela@gmail.com>
2024-03-27 09:26:51 -0400

    refactor rename rand to response_id throughout ChatJob and templates for clarity as to what its used for

--- app/jobs/chat_job.rb
+++ app/jobs/chat_job.rb
@@ -4,24 +4,22 @@ class ChatJob < ApplicationJob
   queue_as :default
 
   def perform(prompt, chat_id)
+    rand = SecureRandom.hex(10)
+    response_id = "response_#{rand}"
+    broadcast_response_container("messages", response_id, chat_id)
+
     cached_context = Rails.cache.read("context_#{chat_id}")
     client = Ollama::Client.new
-    rand = SecureRandom.hex(10)
-    broadcast_response_container("messages", rand, chat_id)
-
     client.request(prompt, cached_context) do |chunk|
-      process_chunk(chunk, rand, chat_id)
+      process_chunk(chunk, response_id, chat_id)
     end
   end
 
   private
 
-  def process_chunk(chunk, rand, chat_id)
+  def process_chunk(chunk, response_id, chat_id)
     json = JSON.parse(chunk)
     done = json["done"]
-    # If response attribute is an empty string, generate html line break
-    # Maybe not needed given use of marked js library?
-    message = json["response"].to_s.strip.empty? ? "<br/>" : json["response"]
     if done
       Rails.logger.info("🎉 Done streaming response for chat_id #{chat_id}.")
 
@@ -29,25 +27,27 @@ class ChatJob < ApplicationJob
       context = json["context"]
       cache_key = "context_#{chat_id}"
       Rails.cache.write(cache_key, context)
-      broadcast_markdown_updater(rand, chat_id)
+      broadcast_markdown_updater(response_id, chat_id)
     else
-      broadcast_response_chunk(rand, message, chat_id)
+      message = json["response"].to_s.strip.empty? ? "<br/>" : json["response"]
+      broadcast_response_chunk(response_id, message, chat_id)
     end
   end
 
-  def broadcast_response_container(target, rand, chat_id)
+  def broadcast_response_container(target, response_id, chat_id)
     Turbo::StreamsChannel.broadcast_append_to [chat_id, "welcome"], target:, partial: "chats/response",
-                                                                    locals: { rand: }
+                                                                    locals: { response_id: }
   end
 
-  def broadcast_response_chunk(target, message, chat_id)
-    Turbo::StreamsChannel.broadcast_append_to [chat_id, "welcome"], target:, html: message
+  def broadcast_response_chunk(response_id, message, chat_id)
+    Turbo::StreamsChannel.broadcast_append_to [chat_id, "welcome"], target: response_id, html: message
   end
 
-  def broadcast_markdown_updater(target, chat_id)
-    Turbo::StreamsChannel.broadcast_append_to [chat_id, "welcome"], target:, partial: "chats/markdown_updater",
+  def broadcast_markdown_updater(response_id, chat_id)
+    Turbo::StreamsChannel.broadcast_append_to [chat_id, "welcome"], target: response_id,
+                                                                    partial: "chats/markdown_updater",
                                                                     locals: {
-                                                                      rand: target,
+                                                                      response_id:,
                                                                       cur_time: Time.current.to_f
                                                                     }
   end
